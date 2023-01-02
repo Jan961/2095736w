cbd253205fa1a0696288e0b40a697728eb0527a0
Jan961 <118180296+Jan961@users.noreply.github.com>
2023-01-02 13:23:10 +0000

    fixing the CI pipeline's data loading

--- hdimvis/data_fetchers/low_lvl_fetchers/FmnistFetcher.py
+++ hdimvis/data_fetchers/low_lvl_fetchers/FmnistFetcher.py
@@ -22,13 +22,21 @@ rng = np.random.default_rng()
 class FmnistFetcher(LowLevelDataFetcherBase):
 
     def load_dataset(self, size: int=1000):
-        XY = np.genfromtxt(os.path.join(DATA_ROOT, 'fashion-mnist_train.csv'), delimiter=",", skip_header=1)
-        sample_indx = rng.choice(60000, size=size, replace=False)
-        sample = XY[sample_indx]
-        labels = sample[:,0]
-        data = sample[:,1:]
+        if os.path.exists(os.path.join(DATA_ROOT, 'fashion-mnist_train.csv')):
+            XY = np.genfromtxt(os.path.join(DATA_ROOT, 'fashion-mnist_train.csv'), delimiter=",", skip_header=1)
+            sample_indx = rng.choice(60000, size=size, replace=False)
+            sample = XY[sample_indx]
+            labels = sample[:,0]
+            data = sample[:,1:]/ 255.0
+
+        else:
+            os.system('git clone https://github.com/zalandoresearch/fashion-mnist.git fashion_mnist')
+            import fashion_mnist.utils.mnist_reader as mnist_reader
+
+            train, train_labels = mnist_reader.load_mnist('fashion-mnist/data/fashion', kind='train')
+            sample_indx = rng.choice(60000, size=size, replace=False)
+            data = np.array(np.vstack(train), dtype=np.float64, order='C')[sample_indx] / 255.0
+            labels = np.array(np.hstack(train_labels))[sample_indx]
 
         return data, labels
 
-
-f = FmnistFetcher()
