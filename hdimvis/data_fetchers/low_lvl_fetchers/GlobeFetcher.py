65d61962a994380e83dd1f33d0d6a4bd3a18b548
Jan961 <118180296+Jan961@users.noreply.github.com>
2023-03-12 23:09:18 +0000

    colours for globe

--- hdimvis/data_fetchers/low_lvl_fetchers/GlobeFetcher.py
+++ hdimvis/data_fetchers/low_lvl_fetchers/GlobeFetcher.py
@@ -6,6 +6,11 @@ from skimage.io import imread
 from ..config import DATA_ROOT
 from pathlib import Path
 
+# code to create the continent png images used by this fetcher with cartopy and the idea of a using 3d points
+# sampled from a world map - from https://github.com/NikolayOskolkov/tSNE_vs_UMAP_GlobalStructure
+# 3d globe (rather than a 2d map rolled into a "swiss roll" as in the code above) and its implementation mine
+
+
 
 class GlobeFetcher(LowLevelDataFetcherBase):
 
@@ -19,7 +24,7 @@ class GlobeFetcher(LowLevelDataFetcherBase):
         matplotlib.rcParams.update({'font.size': 22})
 
         labels = [1, 2, 3, 4, 5, 6]
-        colours = []
+        colours = ['yellow', 'limegreen', 'r', 'b','fuchsia', 'orange' ]
         sizes = []
 
         for i, continent_image in enumerate(self.continents):
@@ -27,7 +32,7 @@ class GlobeFetcher(LowLevelDataFetcherBase):
 
             continent_arr_bw = np.flip(np.where(image_arr[:, :, 0] < 1, 1, 0), axis=0)
             land_points = np.argwhere(continent_arr_bw)
-            plt.scatter(land_points[:, 1], land_points[:, 0], s=0.5)
+            plt.scatter(land_points[:, 1], land_points[:, 0], s=0.5, c=colours[i])
 
             if continent_image == 'Asia.png':
                 zero_meridian = (np.nonzero(continent_arr_bw)[1].max() + np.nonzero(continent_arr_bw)[1].min()) / 2
@@ -44,49 +49,3 @@ class GlobeFetcher(LowLevelDataFetcherBase):
         return imread(image_path)
 
 
-
-
-
-
-# rng = np.random.RandomState(123)
-# plt.figure(figsize=(20, 8))
-# matplotlib.rcParams.update({'font.size': 22})
-
-
-
-# for i, continent_image in enumerate(continents):
-#     continent_arr = imread(continent_image) / 255
-#     continent_arr_bw = np.flip(np.where(continent_arr[:, :, 0] < 1, 1, 0), axis=0)
-#     land_points = np.argwhere(continent_arr_bw)
-#     plt.scatter(land_points[:, 1], land_points[:, 0], s=0.5)
-#
-#     if continent_image == 'Eurasia.png':
-#         zero_meridian = (np.nonzero(continent_arr_bw)[1].max() + np.nonzero(continent_arr_bw)[1].min()) / 2
-#         equator = continent_arr_bw.shape[0] / 2
-#
-# plt.axvline(x=zero_meridian, c='black')
-# plt.axhline(y=equator, c='black')
-# plt.show()
-
-# world_map_rgb_squashed = np.round(np.sum(world_map[:,:,0:3], axis=2)/3, decimals=1)
-# world_map_gray = np.flip(np.where(world_map_rgb_squashed<1, 1, 0), axis=0)
-# plt.imshow(world_map_rgb_squashed)
-
-# plt.imshow(np.where(world_map_rgb_squashed==0.3, 0, 1))
-
-# # 0.2 africa
-
-# # 0.3 america and outline of africa
-
-# print(np.unique(world_map_rgb_squashed))
-
-# zero_meridian = (np.nonzero(world_map_gray)[1].max() + np.nonzero(world_map_gray)[1].min())/2
-# print(f"zero: {zero_meridian}")
-
-
-# land_points = np.argwhere(world_map_gray)
-
-# plt.figure(figsize = (20, 8))
-# plt.scatter(land_points[:,1], land_points[:,0], s=1)
-# plt.axvline(x=zero_meridian, c='r')
-# plt.show()
