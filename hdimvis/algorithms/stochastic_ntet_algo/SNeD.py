bug fixes overflow etc

hdimvis/algorithms/stochastic_ntet_algo/SNeD.py
@@ -76,24 +76,12 @@ class SNeD(BaseAlgorithm):
             Dld_full_matrix, Dld_quartet = compute_quartet_dld(LD_points)
 
             if self.is_test:
-                Dhd_quartet_alt = Dhd_quartet[np.nonzero(Dhd_quartet)] # convert to the form used by the og code
-                assert np.allclose(Dhd_quartet_alt, original_dhd_calculation(exaggerate_dist, HD_points))
-                print("HD distance equality assertion passed")
-
-                # worth keeping in mind that the conversion in the first line above and similar conversions
-                # below are not perfect since if we are very, very unluckily some relevant distances
-                #  in the Dhd_quartet matrix might be zero
-                # but it should be perfectly fine for testing - for perfect conversion see the one in
-                # metrics.distance_measures.rbf_distance
-
-                Dld_quartet_alt = Dld_quartet[np.nonzero(Dld_quartet)]  # convert to the form used by the og code
-                assert np.allclose(Dld_quartet_alt, original_dld_calculation(LD_points))
-                print("LD distance equality assertion passed")
+                self._test_dist_calc( Dhd_quartet, Dld_quartet, HD_points, LD_points, exaggerate_dist)
 
             # after the below couple of lines the Dhd_quartet contains the relative distances
             # the distances in Dld_quartet are NOT relative and are passed as such to the compute_quartet_grads() funct
             if self.use_rbf_adjustment:
-                Dhd_quartet = relative_rbf_dists(Dhd_quartet)
+                Dhd_quartet = relative_rbf_dists(Dhd_quartet, self.ntet_size)
 
             else:
                 Dhd_quartet /= np.sum(Dhd_quartet)
@@ -102,10 +90,7 @@ class SNeD(BaseAlgorithm):
                                                   Dld_quartet, Dld_full_matrix)
 
             if self.is_test:
-                Dhd_1dim = Dhd_quartet[np.nonzero(Dhd_quartet)]  # convert to the format used by the OG grad computation
-                Dld_1dim = Dhd_quartet[np.nonzero(Dhd_quartet)]
-                assert np.allclose(quartet_grads.ravel(), compute_quartet_grads_original(LD_points, Dhd_1dim, Dld_1dim))
-                print("Gradient equality assertion passed")
+                self._test_grad_calc(LD_points, Dhd_quartet, quartet_grads)
 
             if self.use_nesterovs_momentum:
                 self.nesterovs_v[quartet] = self.nesterovs_v[quartet]* self.momentum - LR*quartet_grads
@@ -127,6 +112,27 @@ class SNeD(BaseAlgorithm):
 
     def get_average_quartet_stress(self):
         return self.last_average_quartet_stress_measurement
+
+    def _test_dist_calc(self, Dhd_quartet, Dld_quartet, HD_points, LD_points, exaggerate_dist):
+        Dhd_quartet_alt = Dhd_quartet[np.nonzero(Dhd_quartet)]  # convert to the form used by the og code
+        assert np.allclose(Dhd_quartet_alt, original_dhd_calculation(exaggerate_dist, HD_points))
+        print("HD distance equality assertion passed")
+
+        # worth keeping in mind that the conversion in the first line above and similar conversions
+        # below are not perfect since if we are very, very unluckily some relevant distances
+        #  in the Dhd_quartet matrix might be zero
+        # but it should be perfectly fine for testing - for perfect conversion see the one in
+        # metrics.distance_measures.rbf_distance
+
+        Dld_quartet_alt = Dld_quartet[np.nonzero(Dld_quartet)]  # convert to the form used by the og code
+        assert np.allclose(Dld_quartet_alt, original_dld_calculation(LD_points))
+        print("LD distance equality assertion passed")
+
+    def _test_grad_calc(self, LD_points, Dhd_quartet, quartet_grads):
+        Dhd_1dim = Dhd_quartet[np.nonzero(Dhd_quartet)]  # convert to the format used by the OG grad computation
+        Dld_1dim = Dhd_quartet[np.nonzero(Dhd_quartet)]
+        assert np.allclose(quartet_grads.ravel(), compute_quartet_grads_original(LD_points, Dhd_1dim, Dld_1dim))
+        print("Gradient equality assertion passed")
 
 
 

hdimvis/create_low_d_layout/SQuaDLayout.py
@@ -28,7 +28,7 @@ class SQuaDLayout(LowDLayoutBase):
 
         print(f" \"N-tet\" size: {self.algorithm.ntet_size}")
 
-        calculate_quartet_stress = False
+
         bar = IncrementalBar("Creating layout", max=self.no_iters)
         decay = self.decay if self.decay is not None else np.exp(np.log(1e-3) / self.no_iters)
         if self.exaggerate_D:  # exaggeration of HD distances by squaring
@@ -37,21 +37,28 @@ class SQuaDLayout(LowDLayoutBase):
             stop_d_exa = 0
 
         for i in range(self.no_iters):
+            print(self.iteration_number)
+            calculate_quartet_stress = False
+
+            # the below conditional allows us to avoid calculating avg quartet stress (which is best
+            # measured alongside other calculations during one iteration run)  on every iteration
             if self.optional_metric_collection is not None:
-                if self.iteration_number == self.no_iters:
+                if i == self.no_iters - 1:
                     calculate_quartet_stress = True
 
                 if self.optional_metric_collection.get('Average quartet stress') and \
-                        self.iteration_number % self.optional_metric_collection['Average quartet stress'] == 0 :
+                        self._check_collection_interval('Average quartet stress') :
                     calculate_quartet_stress = True
 
             if i == stop_d_exa:
                 self.LR *= decay
-                exaggerate_D = False
+                self.exaggerate_D = False
 
-            self.algorithm.one_iteration(exaggerate_D, self.LR, calculate_quartet_stress)
+            print(calculate_quartet_stress)
+            self.algorithm.one_iteration(self.exaggerate_D, self.LR, calculate_quartet_stress)
             if self.optional_metric_collection is not None:
                 self.collect_metrics()
+
             bar.next()
             self.iteration_number += 1
             self.final_positions = self.algorithm.get_positions()

hdimvis/metrics/distance_measures/relative_rbf_dists.py
@@ -2,15 +2,17 @@ import numpy as np
 
 # this is just an adjustment - DO NOT use the "distance_function" parameter for algorithms
 # set the "use_rbf_adjustment" to true for Sned if required instead
-def relative_rbf_dists(Dhd_quartet: np.ndarray):
+def relative_rbf_dists(Dhd_quartet: np.ndarray, ntet_size: int):
 
-    Dhd_quartet_triu_entries =  Dhd_quartet.copy()
-    Dhd_quartet_triu_entries[np.tril_indices(4)] = np.nan
+    # use np.longdouble because float64 overflows in exp a few lines below
+    # and float128 is not available in this project setup for some reason
+    Dhd_quartet_triu_entries =  np.longdouble(Dhd_quartet.copy())
+    Dhd_quartet_triu_entries[np.tril_indices(ntet_size)] = np.nan
     Dhd_quartet_triu_entries = Dhd_quartet_triu_entries[np.nonzero(np.invert(np.isnan(Dhd_quartet_triu_entries)))]
     # Dhd_quartet_triu_entries = flattened entries of the upper triangular matrix
 
     std = np.std(Dhd_quartet_triu_entries) + 1e-13 # small number just in case - to avoid zero division
-    rel_dists = np.exp((Dhd_quartet-np.min(Dhd_quartet_triu_entries)) / (-2*std))
+    rel_dists = np.exp((np.longdouble(Dhd_quartet)-np.min(Dhd_quartet_triu_entries)) / (-2*std))
     rel_dists = 1 - rel_dists
 
     np.fill_diagonal(rel_dists, 0)

tests/example_squad.py
@@ -24,12 +24,12 @@ Xld = PCA(n_components=2, whiten=False, copy=True).fit_transform(dataset.data).a
 Xld *= 10/np.std(Xld)
 
 squad = SNeD(dataset=dataset, initial_layout=Xld, use_nesterovs_momentum=False, ntet_size=4,  )
-layout = LowDLayoutCreation().create_layout(squad, optional_metric_collection=metric_collection, no_iters=10)
-# print(layout.collected_metrics)
+layout = LowDLayoutCreation().create_layout(squad, optional_metric_collection=metric_collection, no_iters=2)
+print(layout.collected_metrics)
 show_layouts(layout, use_labels=True, color_map='rainbow', title="  test")
 show_generation_metrics(layout, quartet_stress=True, title="test")
 
-cube.plot_2d(layout=layout, title="test")
+# cube.plot_2d(layout=layout, title="test")
 
 # fig, axis = plt.subplots()
 # axis.scatter(Xld[:,0], Xld[:,1], c=dataset.labels, cmap='rainbow')