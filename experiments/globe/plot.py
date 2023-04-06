3d645f885d8d8608c9c0b3e5f2a5fe4e3f668784
Jan961 <118180296+Jan961@users.noreply.github.com>
2023-04-06 12:00:07 +0100

    factor out globe plotting

--- experiments/globe/plot.py
+++ experiments/globe/plot.py
@@ -3,6 +3,27 @@ import matplotlib.pyplot as plt
 from hdimvis.data_fetchers.Dataset import Dataset
 from pathlib import Path
 
+colours = ['yellow', 'limegreen', 'r', 'b', 'fuchsia', 'orange']
+label_types = [1, 2, 3, 4, 5, 6]
+
+def show_original_globe(globe : Dataset, show_oceans: bool = True):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    for i in label_types:
+        p = globe.data[globe.labels == i]
+        ax.scatter(p[:, 0], p[:, 1], p[:, 2], s=2, c=colours[i - 1])
+
+
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+    ax.zaxis.set_ticklabels([])
+
+    ax.invert_yaxis()
+    ax.set_zlabel("z")
+    ax.set_ylabel("y")
+    ax.set_xlabel("x")
+    plt.show()
+
 
 def show_globe_embedding(dataset: Dataset, points_2d: np.ndarray,
                          color_map = None, title: str = None,
--- hdimvis/data_fetchers/low_lvl_fetchers/GlobeFetcher.py
+++ hdimvis/data_fetchers/low_lvl_fetchers/GlobeFetcher.py
@@ -22,55 +22,23 @@ class GlobeFetcher(LowLevelDataFetcherBase):
     def load_dataset(self, size: int = 10000, **kwargs) -> (np.ndarray, np.ndarray):
 
         sampled_2d_points  = self._get_sample_2d_points(size)
-        print(f"sampled: {sampled_2d_points}")
+        # print(f"sampled: {sampled_2d_points}")
 
         pitch = sampled_2d_points[:, 0]/1  # radius = 1 in this case because of scaling in _get_sample_2d_points
         yaw = sampled_2d_points[:, 1]/1
 
         # print("radius")
         # print(radius)
-
-        print("scaled_points[:, 1")
-        print(sampled_2d_points[:, 1].max())
+        # print("scaled_points[:, 1")
+        # print(sampled_2d_points[:, 1].max())
 
         z = np.sin(pitch)
         x = np.cos(pitch)*np.sin(yaw)
         y = np.cos(pitch) *np.cos(yaw)
-        # x = np.sin(yaw) * np.cos(pitch)
-        # y =  np.sin(yaw) * np.sin(pitch)
-        # z =  np.cos(yaw)
-
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
 
         points_3d = np.vstack((x,y,z)).T
         labels = sampled_2d_points[:,2]
-        print("x")
 
-        longs = np.linspace(0,2*np.pi,50)
-        lats = np.linspace(-np.pi/2,np.pi/2 ,50)
-        z= np.sin(lats)
-        x = np.cos(lats)[:, None] * np.sin(longs)[None, :]
-        y = np.cos(lats)[:, None] * np.cos(longs)[None, :]
-        z = np.tile(z[:, None], (1, 50))
-
-
-
-        colours = ['yellow', 'limegreen', 'r', 'b', 'fuchsia', 'orange']
-        for i in self.labels:
-            p =  points_3d[labels == i]
-            print(f" p{p}")
-            ax.scatter(p[:, 0], p[:, 1],  p[:,2], s=2, c=colours[i-1])
-
-
-        # ax.scatter(x,y,z, zorder=0, s=1)
-
-        # ax.invert_zaxis()
-        ax.invert_yaxis()
-        ax.set_zlabel("z")
-        ax.set_ylabel("y")
-        ax.set_xlabel("x")
-        plt.show()
         return points_3d, labels
 
 
@@ -84,16 +52,12 @@ class GlobeFetcher(LowLevelDataFetcherBase):
         all_two_d_points = []
         for i, continent_image in enumerate(self.continents):
             image_arr = self._get_image_array(continent_image)
-            # fig = plt.figure()
-            # ax = fig.add_subplot()
-            # ax.imshow(image_arr)
-
 
             continent_arr_bw = np.flip(np.where(image_arr[:, :, 0] < 1, 1, 0), axis=0)
             land_points = np.argwhere(continent_arr_bw)
-            fig = plt.figure()
-            ax = fig.add_subplot()
-            ax.scatter(land_points[:,1], land_points[:,0])
+            # fig = plt.figure()
+            # ax = fig.add_subplot()
+            # ax.scatter(land_points[:,1], land_points[:,0])
 
             if i == 0:
                 zero_meridian = continent_arr_bw.shape[1] / 2
@@ -111,31 +75,26 @@ class GlobeFetcher(LowLevelDataFetcherBase):
             sample_indices = np.random.randint(0, points.shape[0], sample_num)
             sampled_points = points[sample_indices]
             labels = np.ones(sample_num) * self.labels[i]
-            print(f" concat{np.concatenate((sampled_points, labels[:, None]), axis=1)}")
+            # print(f" concat{np.concatenate((sampled_points, labels[:, None]), axis=1)}")
             sampled_2d_points.append(np.concatenate((sampled_points, labels[:, None]), axis=1))
 
         points_2d_one_arr = np.vstack(sampled_2d_points)
-        fig = plt.figure()
-        ax = fig.add_subplot()
 
-        # scale x (longitude) to range [-pi, pi] and y to range [-pi/2, pi/2]
+
+        # scale x (longitude) to range [-pi, pi] and y (latitude) to range [-pi/2, pi/2]
         points_2d_one_arr[:, 1] = np.pi * (points_2d_one_arr[:, 1] - zero_meridian)/ (circumference/2)
         points_2d_one_arr[:, 0] = np.pi * (points_2d_one_arr[:, 0] - equator) / (2*(half_meridian_length/2))
 
-        for i in self.labels:
-            p = points_2d_one_arr[points_2d_one_arr[:,2] == self.labels[i-1] ]
-            print(f" p{p}")
-            ax.scatter(p[:, 1], p[:, 0], s=0.5, c=colours[i - 1])
 
-        plt.axvline(x=0, c='black')
-        plt.axhline(y=0, c='black')
-
-        print(f" one aray{points_2d_one_arr}")
-        # min_x = np.min(points_2d_one_arr[:,1])
-        # min_y = np.min(points_2d_one_arr[:, 0])
-        # points_2d_one_arr[:,1]  -= min_x
-        # points_2d_one_arr[:, 0] -= min_y
-        # radius1 =  radius/np.max(points_2d_one_arr[:,1])
+        # fig = plt.figure()
+        # ax = fig.add_subplot()
+        # for i in self.labels:
+        #     p = points_2d_one_arr[points_2d_one_arr[:,2] == self.labels[i-1] ]
+        #     print(f" p{p}")
+        #     ax.scatter(p[:, 1], p[:, 0], s=0.5, c=colours[i - 1])
+        #
+        # plt.axvline(x=0, c='black')
+        # plt.axhline(y=0, c='black')
 
         return points_2d_one_arr
 
